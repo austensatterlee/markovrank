2f8b27f54c462fcb978bbce0c3bb43c5ea7a458b
Austen Satterlee <austen520@gmail.com>
2015-03-23 09:56:00 -0700

    Generalized rankKeywords to scrape n-grams by default.

--- makegraph.py
+++ makegraph.py
@@ -6,22 +6,20 @@ import argparse,traceback,time
 from collections import Counter,deque
 from urlparse import urlparse,urljoin,urldefrag
 
-bad_urls = ['http://bgess.berkeley.edu/%7Ensbejr/index.html','http://superior.berkeley.edu/Berkeley/Buildings/soda.html','http://physicalplant.berkeley.edu/','http://www.eecs.berkeley.edu/Deptonly/Rosters/roster.room.cory.html',' http://www.eecs.berkeley.edu/Resguide/admin.shtml#aliases','http://www.eecs.berkeley.edu/department/EECSbrochure/c1-s3.html']
 bad_filenames=['login.html','admin.html']
 acceptable_extensions=['.html','.htm','.php','.asp','.aspx','']
 acceptable_schemes=['http']
 
 def generatepatterns():
-    make_conditional=lambda lst:'|'.join(map(re.escape,lst))
+    make_disjunction=lambda lst:'|'.join(map(re.escape,lst))
     patterns=[]
-    patterns.append("^[^?]*\/.*?({})(?:\?.*)?$".format(make_conditional(acceptable_extensions)))
-    patterns.append("^({}).*$".format(make_conditional(acceptable_schemes)))
-    patterns.append("^[^?]*\/(?!{})(?:\?.*)?$".format(make_conditional(bad_filenames)))
+#    patterns.append("^.*\/({})$".format(make_disjunction(acceptable_extensions)))
+    patterns.append("^({}).*$".format(make_disjunction(acceptable_schemes)))
     return patterns
 
 def scrape(**kwargs):
     """
-    scrape(url,patterns,maxdepth=1,verbose=0])
+    scrape(url,patterns,maxdepth=1,verbose=0,timeout=None,cachefile=None,reset_cache=False)
 
     Keyword Arguments:
     url - url to start scraping from
@@ -32,6 +30,8 @@ def scrape(**kwargs):
 
     __pagecache__ = readCache(kwargs.get('cachefile'),**kwargs)
     patterns.extend(generatepatterns())
+    if verbose>3:
+        print "Using patterns: {}".format(patterns)
     patterns=[re.compile(p,re.I) for p in patterns]
     history=set()
     targetlist=[]
@@ -53,7 +53,7 @@ def scrape(**kwargs):
                     text = page.text
                     __pagecache__[currnode] = text
                     # Extract target links from current node
-                except requests.ConnectionError,e:
+                except requests.RequestException,e:
                     errorpages.add(currnode)
                     print "Connection error: {}".format(currnode)
                     continue
@@ -72,11 +72,16 @@ def scrape(**kwargs):
 
             # add suitable neighbors to stack
             for link in links:
-                patternmatch=[p.match(link) for p in patterns]
+                parsedlink=urlparse(link)
+                path,ext = os.path.splitext(parsedlink.path)
+                path,filename = os.path.split(path.strip('/'))
+                filename+=ext
+                patternmatch=[True if p.match(link) else False for p in patterns]
+                patternmatch.append(ext in acceptable_extensions)
+                patternmatch.append(filename not in bad_filenames)
                 if not np.all(patternmatch):
                     continue
-                if link not in adjdict[currnode]:
-                    adjdict[currnode].append(link)
+                adjdict[currnode].append(link)
                 if link not in history and link not in stack and currdepth+1<=maxdepth:
                     if verbose>2:
                         print "Link match: {}".format(link.encode('utf','ignore'))
@@ -152,61 +157,89 @@ def writeCache(*args,**kwargs):
         print "Failure to write cachefile to: {}. Found this message: {}".format(cachefile,e)
 
 
-def generateTransitionMatrix(adjdict):
-    nodes=dict(zip(adjdict.keys(),np.arange(len(adjdict))))
-    adjmat=np.zeros((len(nodes),len(nodes)))
+def generateTransitionMatrix(adjdict,normalizerows=True):
+    nodemap=dict(zip(adjdict.keys(),np.arange(len(adjdict))))
+    nodes=np.array([k for k in nodemap])
+    adjmat=np.zeros((len(nodemap),len(nodes)))
     i=0
     slices=[]
-    for node in nodes:
+    for node in nodemap:
         for node2 in adjdict[node]:
-            if node2 not in nodes:
+            if node2 not in nodemap:
                 continue
-            j=nodes[node2]
-            adjmat[i,j]=1
+            j=nodemap[node2]
+            adjmat[i,j]+=1
         i+=1
     # remove states without transitions
-    nonemptystates=[1]
-    while any(nonemptystates)>0:
-        nonemptystates = np.all(np.isnan(adjmat) | np.equal(adjmat, 0), axis=1)
-        adjmat = adjmat[~nonemptystates,:][:,~nonemptystates]
-    adjmat = adjmat/adjmat.sum(axis=1,keepdims=True)
-    return adjmat
+    emptystates=[1]
+    while any(emptystates)>0:
+        emptystates = np.all(np.isnan(adjmat) | np.equal(adjmat, 0), axis=1)
+        nodes = nodes[~emptystates]
+        adjmat = adjmat[~emptystates,:][:,~emptystates]
+    if normalizerows:
+        adjmat = adjmat/adjmat.sum(axis=1,keepdims=True)
+    return adjmat,nodes
 
 
-def rankProfessors(adjlist,ssprobs,keywords,**kwargs):
+def rankKeywords(pagelist,ssprobs,keywords=[],**kwargs):
+    """
+    rankKeywords(pagelist,ssprobs,keywords=[],**kwargs)
+
+    If the keywords argument is left blank, this function can be used to scrape and rank n-grams
+
+    Parameters
+    ---------
+    numletters - the minimum number of letters required for a word to be included in the n-gram
+    numwords - the 'n' in 'n-gram'
+
+    """
     #Uses the code provided to search each url for each professor and update
     #the dictionary according to the rank of each page rather than simply adding one
     __pagecache__=readCache(kwargs.get('cachefile'),**kwargs)
     profs=keywords
-    keywords=[re.compile(k,re.I) for k in keywords]
-    matches={}
+    keywords=[re.compile(r"\b{}\b".format(k),re.I) for k in keywords]
     profdict={}
     for i in profs:
         profdict[i] = 0.
-    url_list=adjlist.keys()
-    for url in url_list:
+    url_list=pagelist
+    all_words={}
+
+    minletters = kwargs.get('numletters',4)
+    numwords = kwargs.get('numwords',1)
+    all_wordpattern=re.compile(' '.join([('(\w{%d,})'%minletters) for x in xrange(numwords)]))
+    for i,url in enumerate(url_list):
         if url in __pagecache__:
             text = __pagecache__[url]
         else:
-            page = requests.get(url,timeout=kwargs.get('timeout'))
-            text = page.text
-            page.close()
-        #for p in profs:
-            #profdict[p] += ssprobs[url_list.index(url)] if " " + p + " " in text else 0
-        for p,pattern in zip(profs,keywords):
-            matches=pattern.findall(text)
-            if not matches:
-                continue
-            nummatches = len(matches)
             try:
-                profdict[p] += ssprobs[url_list.index(url)]*nummatches
-            except:
-                import pdb;pdb.set_trace()
-    prof_ranks = [pair for pair in sorted(profdict.items(), key=lambda item: item[1], reverse=True)]
-    if kwargs.get('verbose')>0:
-        for i in range(len(prof_ranks)):
-            print "%d: %s" % (i+1, prof_ranks[i])
-    return prof_ranks
+                page = requests.get(url,timeout=kwargs.get('timeout'))
+                text = page.text
+                page.close()
+            except requests.RequestException:
+                continue
+        text=BeautifulSoup(text).text
+        if not keywords:
+            for word in all_wordpattern.findall(text):
+                if word not in all_words:
+                    all_words[word]=0
+                all_words[word] += 100*ssprobs[i]
+        else:
+            for p,pattern in zip(profs,keywords):
+                matches=pattern.findall(text)
+                if not matches:
+                    continue
+                nummatches = len(matches)
+                # scale by 100 to avoid decimation
+                profdict[p] += 100*ssprobs[i]*nummatches
+    if keywords:
+        prof_ranks = [pair for pair in sorted(profdict.items(), key=lambda item: item[1], reverse=True)]
+        if kwargs.get('verbose')>0:
+            for i in range(len(prof_ranks)):
+                print "%d: %s" % (i+1, prof_ranks[i])
+        return prof_ranks
+    else:
+        keyword_ranks = all_words
+        return keyword_ranks
 
 def steadystate(transition_matrix):
     """
@@ -245,7 +278,7 @@ def main(**kwargs):
         sys.stdout.write("(elapsed: {}) ".format(elapsed))
         print "Scraped {} pages".format(len(adjlist))
         print "Computing eigenvectors..."
-    pagematrix=generateTransitionMatrix(adjlist)
+    pagematrix,pagelist=generateTransitionMatrix(adjlist)
     tstart=time.clock()
     steadyprobs=steadystate(pagematrix)
     elapsed=time.clock()-tstart
@@ -254,12 +287,13 @@ def main(**kwargs):
         print 'Calculated steady state probabilities: '
         print steadyprobs
     if not kwargs['scrape_only']:
+        kwargs.pop('reset_cache') # don't reset the cache twice
         if kwargs.get('verbose')>0:
             print 'Ranking...'
         if kwargs.get('verbose')>1:
             print 'Keywords... {}'.format(keywords)
         tstart=time.clock()
-        rankings=rankProfessors(adjlist,steadyprobs,**kwargs)
+        rankings=rankKeywords(pagelist,steadyprobs,**kwargs)
         elapsed=time.clock()-tstart
         returndict['rankings']=rankings
         sys.stdout.write("(elapsed: {}) ".format(elapsed))
@@ -274,16 +308,19 @@ def parsearguments(args):
     """
 
     mainParser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,description=descstr)
-    mainParser.add_argument('keywords',type=argparse.FileType('r'),default=sys.stdin,help="file containing keywords to rank")
+    mainParser.add_argument('--keywords',type=argparse.FileType('r'),default=[],help="file containing keywords to rank")
     mainParser.add_argument('-H','--home',type=str,help="the url to start at",default="http://www.eecs.berkeley.edu/Research/Areas/")
-    mainParser.add_argument('-p','--patterns',type=str,help="a list of regular expressions that will be matched against encountered links to determine (in part) whether or not they should be followed",default=[".*\.eecs.berkeley.edu/.*"])
+    mainParser.add_argument('-p','--patterns',type=str,help="a list of \
+            regular expressions that will be matched against encountered \
+            links to determine (in part) whether or not they should be \
+            followed",nargs='*',default=[".*\.eecs.berkeley.edu/.*"])
     mainParser.add_argument('-P','--patternfile',help="instead of reading the patterns from the command line, they will be read from the specified file",type=argparse.FileType('r'),default=None)
     mainParser.add_argument('-R','--reset-cache',action="store_true",default=False)
     mainParser.add_argument('-O','--output',type=argparse.FileType('w'),default=None,help='file to store output contents to')
     mainParser.add_argument('-S','--scrape-only',default=False,action="store_true",help='forgoes professor-ranking')
     mainParser.add_argument('-c','--cachefile',type=str,help="location of  file (used to store adj list).",default="__pagecache__.pkl")
     mainParser.add_argument('-d','--maxdepth',default=1,type=int,help="the maximum recursion level")
-    mainParser.add_argument('-t','--timeout',default=None,type=int,help="max seconds to spend loading page before moving on")
+    mainParser.add_argument('-t','--timeout',default=None,type=float,help="max seconds to spend loading page before moving on")
     mainParser.add_argument('-v','--verbose',type=int,default=0,help="set verbosity level (0 and up)")
     mainParser.add_argument('--debug',action='store_true',default=False,help="debug mode")
     parsedargs = mainParser.parse_args(args)
